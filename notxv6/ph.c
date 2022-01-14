c82f9b9b8700a87adb4530abd2aa4fda3f6702fc
Jack <1779626055@qq.com>
2022-01-14 16:59:47 +0800

    Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,14 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+// 给每一个bucket加锁
+pthread_mutex_t lock[NBUCKET];
+//初始化锁
+void init_locks(){
+  for(int i=0;i<NBUCKET;i++){
+    pthread_mutex_init(lock+i,NULL); 
+  }
+}
 
 double
 now()
@@ -39,7 +47,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(lock+i);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,19 +61,20 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(lock+i);
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(lock+i);
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(lock+i);
   return e;
 }
 
@@ -114,6 +123,9 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+  // add locks
+  init_locks();
+
 
   //
   // first the puts
